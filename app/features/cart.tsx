2c3a16b94d4848477e1438fc464dc9a1784876ae
sachinduKavi <cst21017@std.uwu.ac.lk>
2024-12-22 12:04:16 +0530

    first commit

---
+++ app/features/cart.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Text } from 'react-native'
+
+export default function cart() {
+  return (
+    <View>
+        <Text>Hello World</Text>
+    </View>
+  )
+}
---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import cart from "./features/cart";
+import { View } from "react-native";
+
+const Stack = createStackNavigator()
+export default function Index() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
